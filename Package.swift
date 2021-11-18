78e40e9417959bbcced482c07e516da3d0225a80
Steve Myers <steve@notmandatory.org>
2021-11-17 22:03:09 -0800

    Add bdkFFI.xcframework and fix test

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "BitcoinDevKit",
+    platforms: [
+            .iOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BitcoinDevKit",
-            targets: ["BitcoinDevKit"]),
+            targets: ["bdkFFI", "BitcoinDevKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,9 +21,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "bdkFFI",
+            url: "file://bdkFFI.xcframework.zip",
+            checksum: "3cc04eca3e7c6c783378d3193bb0a240e8ec6da69eb7ef589161f0a97b773e99"
+        ),
         .target(
             name: "BitcoinDevKit",
-            dependencies: []),
+            dependencies: ["bdkFFI"]),
         .testTarget(
             name: "BitcoinDevKitTests",
             dependencies: ["BitcoinDevKit"]),
--- Tests/BitcoinDevKitTests/BitcoinDevKitTests.swift
+++ Tests/BitcoinDevKitTests/BitcoinDevKitTests.swift
@@ -2,10 +2,11 @@ import XCTest
 @testable import BitcoinDevKit
 
 final class BitcoinDevKitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BitcoinDevKit().text, "Hello, World!")
+    func testMemoryWalletNewAddress() throws {
+        let desc = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)"
+        let config = DatabaseConfig.memory(junk: "")
+        let wallet = try OfflineWallet.init(descriptor: desc, network: Network.regtest, databaseConfig: config)
+        let address = wallet.getNewAddress()
+        XCTAssertEqual(address, "bcrt1qzg4mckdh50nwdm9hkzq06528rsu73hjxytqkxs")
     }
 }
